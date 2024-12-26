8e2a27765fc456734a2116e3011e43e1203dd783
unknown <hmstech11@gmail.com>
2024-12-26 17:18:11 +0500

    new

--- src/routes/agent/auth.router.ts
+++ src/routes/agent/auth.router.ts
@@ -3,5 +3,5 @@ import authenticateUser from '../../middlewares/authenticate.user';
 import { loginController } from '../../controllers/agent/agent.auth.controllers';
 
 const agentauthRouter = express.Router();
-agentauthRouter.post('/login', authenticateUser, loginController);
+agentauthRouter.post('/login',  loginController);
 export default agentauthRouter;
