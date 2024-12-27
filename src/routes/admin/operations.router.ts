aafbd9432625eab39dacff639ab21cb4bf04dd7f
unknown <hmstech11@gmail.com>
2024-12-27 13:41:12 +0500

    new

--- src/routes/admin/operations.router.ts
+++ src/routes/admin/operations.router.ts
@@ -171,7 +171,7 @@ operationsRouter.get(
 
 operationsRouter.get('/get-all-transactions', authenticateUser,getAllTrsansactions);
 operationsRouter.get('/get-dashboard-stats', authenticateUser,getAdminDashboardStats);
-operationsRouter.get('/get-customer-stats', authenticateUser,getAdminDashboardStats);
+// operationsRouter.get('/get-customer-stats', authenticateUser,getAdminDashboardStats);
 operationsRouter.get('/get-customer-transactions/:id', authenticateUser,getTransactionForCustomer);
 operationsRouter.get('/get-customer-stats', authenticateUser,getCustomerStats);
 export default operationsRouter;
