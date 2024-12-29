4377c54655e7057936ce0f42006e27edca25cfe1
unknown <hmstech11@gmail.com>
2024-12-29 20:11:20 +0500

    bew

--- src/routes/cutomer/auth.router.ts
+++ src/routes/cutomer/auth.router.ts
@@ -20,15 +20,15 @@ import upload from '../../middlewares/multer.middleware';
 // import authController from '../controllers/auth.controller';
 const authRouter = express.Router();
 
-authRouter.post('/customer/register', upload.single('profilePicture'),registerCustomerController);
+authRouter.post('/customer/register', upload.single('profilePicture'), registerCustomerController);
 authRouter.post('/logout', logoutController);
 authRouter.post('/verify-email-otp', authenticateUser, verifyUserController);
 authRouter.post('/verify-forgot-password-otp', verifyForgotPasswordOtp);
 authRouter.post('/resend-otp', resendOtpController);
 authRouter.post('/forgot-password', sendPasswordOtpController);
-authRouter.post('/edit-profile',authenticateUser,editProfileController)
-authRouter.post('/change-password',authenticateUser,changePasswordController)
-authRouter.post('/kyc-request',authenticateUser,kycTierTwoRequest);
-authRouter.get('/get-all-notifications',authenticateUser,getAllNotifcications);
-authRouter.post('/set-new-password',setNewPasswordController);
+authRouter.post('/edit-profile', upload.single('profilePicture'), authenticateUser, editProfileController)
+authRouter.post('/change-password', authenticateUser, changePasswordController)
+authRouter.post('/kyc-request', authenticateUser, kycTierTwoRequest);
+authRouter.get('/get-all-notifications', authenticateUser, getAllNotifcications);
+authRouter.post('/set-new-password', setNewPasswordController);
 export default authRouter;
